040a67f37a89984f33002fd4f00724fcddc1e50e
naik-yogi <naikyogesh62@gmail.com>
2024-11-02 19:07:04 +0530

    maven project with all dependencies and webdriver manager

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager/5.9.2");
+        driver.quit();
+    }
+}
